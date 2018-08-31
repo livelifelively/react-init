e79ec3dfd9a84570ec51dc7991a64b829d5e442a
ashish rana <livelifelively@ashishs-MacBook-Pro.local>
2018-08-31 08:11:23 +0530

    storeFactory; findByDataTestAttribute

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,10 +1,25 @@
+import checkPropTypes from 'check-prop-types';
+import { createStore } from 'redux';
+
+import rootReducers from '../src/reducers';
+
+/**
+ * Create a testing store with imported reducers, middlewares and initial state
+ * @globals: rootReducers
+ * @param initialState
+ * @returns {*}
+ */
+export const storeFactory = (initialState) => {
+  return createStore(rootReducers, initialState);
+};
+
 /**
  * @function findByDataTestAttr - find elements by their data-test value
  * @param {ShallowWrapper} wrapper
  * @param {string}  attr
  * @returns {ShallowWraper}
  */
-export const findByDataTestAttr = (wrapper, attr) => {
+export const findByDataTestAttribute = (wrapper, attr) => {
   return wrapper.find(`[data-test="${attr}"]`);
 };
 
