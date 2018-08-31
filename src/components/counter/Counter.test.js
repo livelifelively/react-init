59c70ce69e2fb9c805318313a10247267f06b11a
ashish rana <livelifelively@ashishs-MacBook-Pro.local>
2018-08-31 08:10:27 +0530

    change function name

--- src/components/counter/Counter.test.js
+++ src/components/counter/Counter.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 
-import {findByDataTestAttr, checkProps} from "../../../test/testUtils";
+import {findByDataTestAttribute, checkProps} from "../../../test/testUtils";
 import Counter from './Counter';
 
 /**
@@ -24,37 +24,37 @@ describe('Counter Component', () => {
 
     test('successfully render component', () => {
       const wrapper = setup();
-      const counterComponent = findByDataTestAttr(wrapper, 'component-counter');
+      const counterComponent = findByDataTestAttribute(wrapper, 'component-counter');
       expect(counterComponent.length).toBe(1);
     });
 
     test('successfully render counter display', () => {
       const wrapper = setup();
-      const counterDisplay = findByDataTestAttr(wrapper, 'counter-display');
+      const counterDisplay = findByDataTestAttribute(wrapper, 'counter-display');
       expect(counterDisplay.length).toBe(1);
     });
 
     test('successfully render increase counter button', () => {
       const wrapper = setup();
-      const counterIncrementButton = findByDataTestAttr(wrapper, 'counter-increment-button');
+      const counterIncrementButton = findByDataTestAttribute(wrapper, 'counter-increment-button');
       expect(counterIncrementButton.length).toBe(1);
     });
 
     test('successfully render decrease counter button', () => {
       const wrapper = setup();
-      const counterDecrementButton = findByDataTestAttr(wrapper, 'counter-decrement-button');
+      const counterDecrementButton = findByDataTestAttribute(wrapper, 'counter-decrement-button');
       expect(counterDecrementButton.length).toBe(1);
     });
 
     test('successfully displays 0 on counter display', () => {
       const wrapper = setup();
-      const counterDisplay = findByDataTestAttr(wrapper, 'counter-display');
+      const counterDisplay = findByDataTestAttribute(wrapper, 'counter-display');
       expect(counterDisplay.text()).toContain(0);
     });
 
     test('successfully displays error message', () => {
       const wrapper = setup({}, {counter: 0, error: 'error here'});
-      const errorDisplay = findByDataTestAttr(wrapper, 'counter-error');
+      const errorDisplay = findByDataTestAttribute(wrapper, 'counter-error');
       expect(errorDisplay.text().length).not.toBe(0);
     });
   });
@@ -63,44 +63,44 @@ describe('Counter Component', () => {
     test('counter value increases on clicking increment button', () => {
       const counter = 8;
       const wrapper = setup({}, {counter});
-      const incrementButton = findByDataTestAttr(wrapper, 'counter-increment-button');
+      const incrementButton = findByDataTestAttribute(wrapper, 'counter-increment-button');
       incrementButton.simulate('click');
       wrapper.update();
 
-      const counterDisplay = findByDataTestAttr(wrapper, 'counter-display');
+      const counterDisplay = findByDataTestAttribute(wrapper, 'counter-display');
       expect(counterDisplay.text()).toContain(counter+1);
     });
 
     test('counter value decreases on clicking decrement button', () => {
       const counter = 8;
       const wrapper = setup({}, {counter});
-      const decrementButton = findByDataTestAttr(wrapper, 'counter-decrement-button');
+      const decrementButton = findByDataTestAttribute(wrapper, 'counter-decrement-button');
       decrementButton.simulate('click');
       wrapper.update();
 
-      const counterDisplay = findByDataTestAttr(wrapper, 'counter-display');
+      const counterDisplay = findByDataTestAttribute(wrapper, 'counter-display');
       expect(counterDisplay.text()).toContain(counter-1);
     });
 
     test('counter does not go below 0', () => {
       const counter = 0;
       const wrapper = setup({}, {counter});
-      const decrementButton = findByDataTestAttr(wrapper, 'counter-decrement-button');
+      const decrementButton = findByDataTestAttribute(wrapper, 'counter-decrement-button');
       decrementButton.simulate('click');
       wrapper.update();
 
-      const counterDisplay = findByDataTestAttr(wrapper, 'counter-display');
+      const counterDisplay = findByDataTestAttribute(wrapper, 'counter-display');
       expect(counterDisplay.text()).toContain(counter);
     });
 
     test('counter shows error when decrement button clicked at 0', () => {
       const counter = 0;
       const wrapper = setup({}, {counter});
-      const decrementButton = findByDataTestAttr(wrapper, 'counter-decrement-button');
+      const decrementButton = findByDataTestAttribute(wrapper, 'counter-decrement-button');
       decrementButton.simulate('click');
       wrapper.update();
 
-      const errorDisplay = findByDataTestAttr(wrapper, 'counter-error');
+      const errorDisplay = findByDataTestAttribute(wrapper, 'counter-error');
       expect(errorDisplay.text().length).not.toBe(0);
     });
 
@@ -108,11 +108,11 @@ describe('Counter Component', () => {
       const counter = 0;
       const error = 'some error here';
       const wrapper = setup({}, {counter, error});
-      const incrementButton = findByDataTestAttr(wrapper, 'counter-increment-button');
+      const incrementButton = findByDataTestAttribute(wrapper, 'counter-increment-button');
       incrementButton.simulate('click');
       wrapper.update();
       // wrapper.state()
-      const errorDisplay = findByDataTestAttr(wrapper, 'counter-error');
+      const errorDisplay = findByDataTestAttribute(wrapper, 'counter-error');
       expect(errorDisplay.length).toBe(0);
     });
 
@@ -120,7 +120,7 @@ describe('Counter Component', () => {
       const counter = 0;
       const error = 'some error here';
       const wrapper = setup({}, {counter, error});
-      const incrementButton = findByDataTestAttr(wrapper, 'counter-increment-button');
+      const incrementButton = findByDataTestAttribute(wrapper, 'counter-increment-button');
       incrementButton.simulate('click');
       wrapper.update();
 
