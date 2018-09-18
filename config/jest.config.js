ab215995a146637971b8ffbd56a90ccdf1346265
ashish rana <livelifelively@ashishs-MacBook-Pro.local>
2018-09-18 12:05:18 +0530

    adapt to move to folder

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  setupTestFrameworkScriptFile: '<rootDir>/test/setupTests.js',
+  setupTestFrameworkScriptFile: '../test/setupTests.js',
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-    "<rootDir>/__mocks__/fileMock.js",
-      "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js"
+    "../__mocks__/fileMock.js",
+      "\\.(css|less)$": "../__mocks__/styleMock.js"
   },
   "testURL": "http://localhost:8080/"
 }
