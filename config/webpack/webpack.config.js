11729ebe973a76314f6e4c827b0e5c507b8ff9c0
ashish rana <livelifelively@ashishs-MacBook-Pro.local>
2018-09-18 12:02:25 +0530

    move configs to config folder;

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -12,6 +12,15 @@ module.exports = {
       {
         test: /\.css$/,
         use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}
+          }
+        ]
       }
     ]
   },
